75a82f9c34839041bcbeb60f664750e8fe01283f
Ben <bigriffin94@outlook.com>
2020-06-16 15:16:56 -0700

    Da-2 MVP MUL alu() operation added and working properly run() updated with MUL LDI command

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -4,6 +4,7 @@ import sys
 LDI = 0b10000010  # LDI R0,8
 PRN = 0b01000111  # PRN R0
 HLT = 0b00000001  # HLT
+MUL = 0b10100010  # MUL R0,R1
 
 
 class CPU:
@@ -16,33 +17,51 @@ class CPU:
         self.running = True  # a variable used to run our RUN repl
         self.reg = [0] * 8
 
-    def load(self):
+    def load(self, filename):
         """Load a program into memory."""
 
         address = 0
 
-        # For now, we've just hardcoded a program:
+        with open(filename) as f:
+            for line in f:
+                # split our enumerated list at any hashmark which indicates a comment
+                line = line.split('#')
+                # print("\/\/ line[0]", line[0])
 
-        program = [
-            # From print8.ls8
-            0b10000010,  # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111,  # PRN R0
-            0b00000000,
-            0b00000001,  # HLT
-        ]
+                try:
+                    # we access line[0] bc this is the 0th index in our enumerated list and we do not want any thing past it.
+                    v = int(line[0], 2)  # working at a base 2 level
+                    # print("    hello v ", v)
+                except ValueError:
+                    continue
 
-        for instruction in program:
-            self.ram[address] = instruction
-            address += 1
+                self.ram[address] = v
+
+                address += 1
+
+        # # For now, we've just hardcoded a program:
+
+        # program = [
+        #     # From print8.ls8
+        #     0b10000010,  # LDI R0,8
+        #     0b00000000,
+        #     0b00001000,
+        #     0b01000111,  # PRN R0
+        #     0b00000000,
+        #     0b00000001,  # HLT
+        # ]
+
+        # for instruction in program:
+        #     self.ram[address] = instruction
+        #     address += 1
 
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
 
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
-        # elif op == "SUB": etc
+        elif op == "MUL":
+            self.reg[reg_a] *= self.reg[reg_b]
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -90,6 +109,7 @@ class CPU:
         while self.running:
             # store our Instruction Register at the place in memory at the index of our current PC
             ir = self.ram[self.pc]
+            # print("IR", ir)
 
             # if our pc is on a LoaD Imediatley index then we need to...
             if ir == LDI:
@@ -110,9 +130,15 @@ class CPU:
                 reg_num = self.ram_read(self.pc + 1)
                 # print out our variable above in order to PRN properly
                 # TODO read from register NOT ram
-                print(self.reg_read(reg_num))
+                print("PRN:", self.reg_read(reg_num))
                 # increment to the next command in the stack past our MAR & MDR
                 self.pc += 2
+
+            elif ir == MUL:
+                reg_a = self.ram_read(self.pc + 1)
+                reg_b = self.ram_read(self.pc + 2)
+                self.alu("MUL", reg_a, reg_b)
+                self.pc += 3
 
             # if we find a HLT command...
             elif ir == HLT:
@@ -123,4 +149,5 @@ class CPU:
 
             # otherwise we want to tell our user what the issue is with the cpu run method
             else:
-                print(f"Unknown expression {ir} at address {pc}")
+                print(f"Unknown expression {ir} at address {self.pc}")
+                sys.exit(1)
